8a9eba5e71f742cbf1c370f1f0b743bfde68eebb
Ludek Suk <ludek.suk@gmail.com>
2020-11-24 12:51:02 +0100

    Script updates

--- python/log2json.py
+++ python/log2json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Author: Ludek Suk
-# Update the FIELD_NAMES and FIELD_DELIMITER below to match your log type
+# Update the FIELD_NAMES and FIELD_DELIMITER to match your log type
 # The sample_log.txt provided with this script is a Palo Alto Threat Log
 # Convert the log file to json format by running: python3 log2json.py > pan-threat-log.json
 
@@ -9,10 +9,10 @@
 # https://stackoverflow.com/questions/54689242/convert-log-file-into-json-file-using-python
 
 import json
+import sys
 
-FIELD_NAMES = ['receive_time', 'serial', 'type', 'subtype', 'time_generated', 'src', 'dst', 'natsrc', 'natdst', 'rule', 'srcuser', 'dstuser', 'app', 'vsys', 'from', 'to', 'inbound_if', 'outbound_if', 'sessionid', 'repeatcnt', 'sport', 'dport', 'natsport', 'natdport', 'flags', 'proto', 'action', 'misc', 'threatid', 'category', 'severity', 'direction', 'seqno', 'actionflags', 'srcloc', 'dstloc', 'contenttype', 'pcap_id', 'filedigest', 'cloud', 'url_idx', 'user_agent', 'filetype', 'xff', 'referer', 'sender', 'subject', 'recipient', 'reportid', 'dg_hier_level', 'vsys_name', 'device_name', 'src_uuid', 'dst_uuid', 'http_method', 'tunnel_id', 'monitortag', 'parent_session_id', 'parent_start_time', 'tunnel', 'contentver', 'assoc_id', 'ppid', 'http_headers']
+FIELD_NAMES = ['receive_time', 'serial', 'type', 'subtype', 'time_generated', 'src', 'dst', 'natsrc', 'natdst', 'rule', 'srcuser', 'dstuser', 'app', 'vsys', 'from', 'to', 'inbound_if', 'outbound_if', 'logset', 'sessionid', 'repeatcnt', 'sport', 'dport', 'natsport', 'natdport', 'flags', 'proto', 'action', 'misc', 'threatid', 'category', 'severity', 'direction', 'seqno', 'actionflags', 'srcloc', 'dstloc', 'contenttype', 'pcap_id', 'filedigest', 'cloud', 'url_idx', 'user_agent', 'filetype', 'xff', 'referer', 'sender', 'subject', 'recipient', 'reportid', 'dg_hier_level', 'vsys_name', 'device_name', 'src_uuid', 'dst_uuid', 'http_method', 'tunnel_id', 'monitortag', 'parent_session_id', 'parent_start_time', 'tunnel', 'contentver', 'assoc_id', 'ppid', 'http_headers']
 FIELD_DELIMITER = ','
-
 
 def log_lines_to_json(log_file, field_names, field_delimiter):
     result = []
@@ -23,6 +23,14 @@ def log_lines_to_json(log_file, field_names, field_delimiter):
             result.append({field_name: fields[idx] for idx, field_name in enumerate(field_names)})
     return result
 
-entries = log_lines_to_json('sample_log.txt', FIELD_NAMES, FIELD_DELIMITER)
-for entry in entries:
-     print(json.dumps(entry))
+try:
+    LOG_FILE = str(sys.argv[1])
+    entries = log_lines_to_json(LOG_FILE, FIELD_NAMES, FIELD_DELIMITER)
+    for entry in entries:
+        print(json.dumps(entry))
+except:
+    print("===USAGE===")
+    print("Update the FIELD_NAMES and FIELD_DELIMITER to match your log type.")
+    print("The sample_log.txt provided with this script is a Palo Alto Threat Log.")
+    print("The correct syntax is: python3 log2json.py log_file_name")
+    sys.exit(1)
